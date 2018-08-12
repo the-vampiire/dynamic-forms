2225b170b50fbb308ddc35c98f58dcbf53135c6a
The-Vampiire <vampiirecodes@gmail.com>
2018-08-12 19:53:20 -0400

    configured App with example Routes

---
+++ user_client/src/App.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Switch, Route } from "react-router-dom";
+
+import {
+  Landing,
+  DynamicForm
+} from "./components";
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Landing} />
+
+    <Route
+      exact path="/form/:purpose?/:version?"
+      render={
+        ({ match: { params: { purpose, version } } }) => (
+          <DynamicForm
+            purpose={purpose || "example"}
+            version={version} // defaults to latest version
+          />
+        )
+      }
+    />
+
+    <Route
+      exact path="/form/qs/:purpose?/:version?"
+      render={
+        ({
+          match: { params: { purpose, version } },
+          location: { search },
+        }) => (
+          <DynamicForm
+            purpose={purpose || "example"}
+            version={version} // defaults to latest version
+            queryString={search}
+          />
+        )
+      }
+    />
+
+    <Route
+      exact path="/form/hidden/:version?"
+      render={
+        ({ match: { params: { version } } }) => (
+          <DynamicFormWrapper
+            version={version} // defaults to latest version
+          />
+        )
+      }
+    />
+  </Switch>
+);
+
+export default App;
