dbccd554ad387747bc33810d847a2d24fb6da013
The-Vampiire <vampiirecodes@gmail.com>
2018-08-13 01:29:39 -0400

    Questions query and resolver

---
+++ api/schema/queries/questions/questions_query.js
@@ -0,0 +1,17 @@
+module.exports = {
+  Query: {
+    Questions: async (
+      root,
+      { input_type, tags, exact_match },
+      { mongo_models: { DynamicQuestion } },
+    ) => {
+      const query = {};
+      // incrementally build up the query object
+      if (input_type) query.input_type = input_type;
+      // search for any tag within the tags array input or for exact match of whole array
+      if (tags) query.tags = exact_match ? tags : { $all: tags };
+
+      return DynamicQuestion.find(query);
+    },
+  },
+};
