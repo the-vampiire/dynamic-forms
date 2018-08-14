b635c61a7c12a81242e8b21a4effdb4cc221b279
The-Vampiire <vampiirecodes@gmail.com>
2018-08-14 16:04:14 -0400

    heavily refactored Dynamic Form utilities. supports new Dynamic Question
    - provides validation for arrays, email, and url
    - simpler interface when creating DynamicQuestions

--- api/models/dynamic_form/utilities.js
+++ api/models/dynamic_form/utilities.js
@@ -1,38 +1,123 @@
 const mongoose = require('mongoose');
+const { isEmail, isURL } = require('validator');
 
 const purposes = require('./purpose_enum');
+/**
+ * DEFAULTS:
+ *  data_type: String
+ *  minlength: 5
+ *  maxlength: 500
+ *  custom validator
+ *    multiple responses - uses min / maxlength if passed
+ *      min_choices : 1
+ *    email: email validator
+ *    url: url validator
+ */
 
-const mapFieldType = (schema) => {
-  const typeMap = {
+// -- IS / HAS -- //
+
+const isArrayType = (input_type) => {
+  switch (input_type) {
+    case 'checkbox':
+    case 'checkbox_2_column':
+    case 'dropdown_multiple':
+      return true;
+    default:
+      return false;
+  }
+}
+
+const hasValidator = (input_type) => {
+  // has 'min_choices' validator
+  if (isArrayType(input_type)) return true;
+
+  return [ 
+    'url', // has url validator
+    'email', // has email validator
+  ].includes(input_type);
+}
+
+// -- MAPPERS -- //
+
+const mapTypeConstructor = (data_type) => {
+  const type_map = {
     Number: Number,
     String: String,
-    Date: Date,
-    Boolean: Boolean,
-    ObjectId: mongoose.Schema.Types.ObjectId,
+
+// DEV TODO currently unsupported
+    // Date: Date,
+    // Boolean: Boolean,
+    // ObjectId: mongoose.Schema.Types.ObjectId,
   };
-
-  const mappedSchema = { ...schema };
-
-  // checks for 'type' top level or nested in an array
-    // type: [{ type: Constructor }] <- nested in array
-    // type: Constructor <- top level
-  const schemaType = Array.isArray(schema.type) ? schema.type[0].type : schema.type;
-
-  // assigns constructors from String: Constructor map (typeMap)
-    // needed because constructor functions do not persist when docs are converted to JSON/BSON
-    // a similar method can be used with a validatorMap for adding field validators
-  if (Array.isArray(schema.type)) { // map array -> type: [{ type: Constructor }]
-    mappedSchema.type[0].type = typeMap[schemaType];
-  } else { // map top level -> type: Constructor
-    mappedSchema.type = typeMap[schemaType];
-  }
-
-  return mappedSchema;
+  return type_map[data_type];
 };
 
-const schemaWithConstraints = (raw_schema, purpose) => {
+const mapValidator = (input_type, minlength) => {
+  const min = minlength || 1;
+
+  const validators = {
+    min_choices: {
+      validator: val => { console.log(val); return val.length >= min},
+      message: `At least ${min} choices are required`,
+    },
+    email: {
+      validator: val => val.isEmail,
+      message: 'Invalid email',
+    },
+    url: {
+      validator: val => val.isURL,
+      message: 'Invalid URL',
+    },
+  }
+
+  return isArrayType(input_type) ?
+    validators['min_choices'] :
+    validators[input_type];
+}
+
+const mapSchema = (
+  { 
+    data_type,
+    input_type,
+    options,
+    minlength,
+    maxlength,
+  },
+) => {
+  const field_schema = {};
+  
+  // sets 'type' of [{ type: Constructor }] or type: Constructor
+  // based on multiple or single response capture
+  field_schema.type = isArrayType(input_type) ?
+    [{ type: mapTypeConstructor(data_type) }] : 
+    mapTypeConstructor(data_type);
+
+  // add text validation for min and max length
+  // default min: 5, default max: 500
+  if (['text', 'textarea'].includes(input_type)) {
+    field_schema.minlength = minlength || 5;
+    field_schema.maxlength = maxlength || 500;
+  }
+  
+  // add custom validators as needed
+  if (hasValidator(input_type)) {
+    field_schema.validate = mapValidator(
+      input_type,
+      minlength,
+    );
+  }
+
+  // set enum if preset 'options' are passed
+  if (options) field_schema.enum = options;
+  
+  return field_schema;
+};
+
+// -- CONSTRAINTS -- //
+
+// returns Mongoose Schema with unique constraints
   // sets single or composite unique constraints based on form purpose
-  // TODO: set custom composite constraints here
+const schemaWithConstraints = (raw_schema, purpose) => {
   let constraint_fields;
   switch (purpose) {
     default:
@@ -46,18 +131,21 @@ const schemaWithConstraints = (raw_schema, purpose) => {
   return constrained_schema;
 };
 
+// -- BASE SCHEMA -- //
+
+// returns a base schema to reduce fields into
 const getSchemaBase = (purpose) => {
   const base = {
     version: {
-      type: 'Number',
+      type: Number,
       required: true,
     },
     purpose: {
-      type: 'String',
+      type: String,
       enum: purposes,
     },
     user_id: {
-      type: 'ObjectId', // change to 'Number' for SQL relationships
+      type: mongoose.Schema.Types.ObjectId, // change to 'Number' for SQL relationships
       required: true,
     },
   };
@@ -65,24 +153,31 @@ const getSchemaBase = (purpose) => {
   // TODO: add additional fields below
     // for specific 'purpose' dynamic forms
   switch (purpose) {
+    // case 'custom':
+      // base.custom_field = custom_field_schema
+      // return base
     default:
       return base;
   }
 };
 
+// -- MAIN EXPORT -- //
+
+// returns a mapped and constrained Mongoose Schema
+// uses Question data
 const createSchema = (questions, purpose) => {
   // DEV-TODO handle min / max length validators for field and array
 
   // reduces questions into a raw schema building on top of base schema
   // formats and adds a schema field for each question
   const raw_schema = questions.reduce(
-    (formSchema, { field_name, schema_data }) => {
+    (formSchema, question ) => {
       const updated_form_schema = { ...formSchema };
 
       // maps the field 'type' Constructors
         // they are stored as String to survive the
         // stringifyng conversion to BSON on Mongo storage
-      updated_form_schema[field_name] = mapFieldType(schema_data);
+      updated_form_schema[question.field_name] = mapSchema(question);
       return updated_form_schema;
     },
     getSchemaBase(purpose), // base schema + custom 'purpose' based fields
