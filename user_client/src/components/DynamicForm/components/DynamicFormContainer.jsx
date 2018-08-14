eb8bfb88b67dcda4a354b3d4a7b108c0ebfcc12a
The-Vampiire <vampiirecodes@gmail.com>
2018-08-14 16:10:42 -0400

    added (LS) persistence to Dynamic Forms

--- user_client/src/components/DynamicForm/components/DynamicFormContainer.jsx
+++ user_client/src/components/DynamicForm/components/DynamicFormContainer.jsx
@@ -13,10 +13,18 @@ export default class DynamicFormContainer extends React.Component {
       purpose,
       version,
       questions,
-      form_data: this.mapFormDataFields(questions), // snake_case for API
+      form_data: this.initializeFormData(purpose, questions),
     }
 
     this.onFormChange = this.onFormChange.bind(this);
+  }
+
+  initializeFormData = (purpose, questions) => {
+    const persistedData = window.localStorage.getItem(purpose);
+    if (persistedData) return JSON.parse(persistedData);
+
+    // if no persisted data is found use default mapping method
+    return this.mapFormDataFields(questions);
   }
 
   mapFormDataFields = (questions) => questions.reduce(
@@ -27,9 +35,8 @@ export default class DynamicFormContainer extends React.Component {
         [
           'checkbox',
           'checkbox_2_column',
-          'dropdown_multiple',
         ].includes(input_type)
-      ) form_data[field_name] = new Set();
+      ) form_data[field_name] = [];
       else form_data[field_name] = '';
 
       // insert hidden field values from hiddenData
@@ -49,9 +56,12 @@ export default class DynamicFormContainer extends React.Component {
     {},
   );
 
-  toggleValueInSet = (set, value) => {
-    set.has(value) ? set.delete(value) : set.add(value);
-    return set;
+  toggleValueInArray = (array, value) => {
+    const clone = array.slice(0);
+    const index = clone.indexOf(value);
+    index !== -1 ? clone.splice(index, 1) : clone.push(value);
+
+    return clone;
   }
 
   onFormChange = ({ currentTarget }) => {
@@ -59,14 +69,23 @@ export default class DynamicFormContainer extends React.Component {
     const form_data = { ...this.state.form_data };
 
     form_data[name] = type === 'checkbox' ?
-      form_data[name] = this.toggleValueInSet(form_data[name], value) :
+      form_data[name] = this.toggleValueInArray(form_data[name], value) :
       form_data[name] = value;
+
+    // persistence in LS
+    window.localStorage.setItem(
+      this.state.purpose,
+      JSON.stringify(form_data),
+    );
 
     this.setState({ form_data });
   }
 
-  onSubmit = (submitMutation, variables) => submitMutation({ variables });
-
+  onSubmit = (submitMutation, variables) => {
+    // clear LS form persistence on submit
+    window.localStorage.removeItem(this.state.purpose);
+    submitMutation({ variables });
+  }
   render() {
     const { purpose, version, questions, form_data } = this.state;
     return (
