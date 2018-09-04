b0cc716fa8a87a56db7d788b1d77ab1b356b49ba
The-Vampiire <vampiirecodes@gmail.com>
2018-09-03 20:56:38 -0400

    persistance flag (LS form data). proptypes and JSDoc descriptions

--- user_client/src/components/DynamicForm/components/DynamicFormWrapper.jsx
+++ user_client/src/components/DynamicForm/components/DynamicFormWrapper.jsx
@@ -4,25 +4,25 @@ import { Redirect } from "react-router-dom";
 
 import Error from "../../Error";
 import DynamicFormContainer from "./DynamicFormContainer";
+import dynamicFormSubmitMutation from "../dynamicFormSubmitMutation.js";
 
 /**
  * @prop {object} client Apollo Client instance for mutating
  * @prop {string} purpose form purpose (database collection)
  * @prop {number} version form version
  * @prop {array} questions form questions
- * 
  * -- OPTIONAL -- 
+ * @prop {bool} persistance controls LS persistence of form data 
  * @prop {object} hiddenData optional object of hidden input field value(s)
- * @prop {func} onResponse optional handler for response data
- * @prop {func} onError optional handler for error data
+ * @prop {func} onValidate optional handler for validating a form field (onChange)
+ * @prop {func} onSubmit optional handler for submitting form
+ * @prop {func} onResponse optional handler for mutation response data
+ * @prop {func} onError optional handler for mutation error message
  */
 class DynamicFormWrapper extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      response: null,
-      error: null,
-    }
+  state = {
+    response: null,
+    error: null,
   }
 
   handleResponse = ({ data: response }) => {
@@ -32,31 +32,31 @@ class DynamicFormWrapper extends React.Component {
     this.setState({ response });
   }
 
-  handleError = ({ message }) => this.setState({ error: message });
+  handleError = error => this.setState({ error });
 
   handleSubmit = (form_data) => {
     const {
       client,
-      mutation,
       purpose,
       version,
     } = this.props;
   
-    const variables = {
-      purpose,
-      version,
-      form_data,
-    };
+    const variables = { purpose, version, form_data };
 
-    client.mutate({ mutation, variables })
-      .then(this.handleResponse)
-      .catch(this.handleError);
+    client.mutate({
+      mutation: dynamicFormSubmitMutation,
+      variables,
+    }).then(this.handleResponse)
+    .catch(this.handleError);
   }
 
   render() {
     const {
+      onValidate,
+      onSubmit,
       onError,
       onResponse,
+      persistance,
       purpose,
       questions,
       hiddenData,
@@ -67,7 +67,7 @@ class DynamicFormWrapper extends React.Component {
     if (error) return (
       onError
         ? onError(error)
-        : <Error error={error} />
+        : <Error error={error.message} />
     );
 
     if (response) return (
@@ -78,10 +78,12 @@ class DynamicFormWrapper extends React.Component {
 
     return (
       <DynamicFormContainer
+        persistance={persistance}
         purpose={purpose}
         questions={questions}
         hiddenData={hiddenData}
-        handleSubmit={this.handleSubmit}
+        onValidate={onValidate}
+        onSubmit={onSubmit || this.handleSubmit}
       />
     );
   }
@@ -91,13 +93,20 @@ DynamicFormWrapper.propTypes = {
   client: PropTypes.object,
   purpose: PropTypes.string,
   version: PropTypes.number,
+  questions: PropTypes.arrayOf(PropTypes.object),
+  persistance: PropTypes.bool,
   hiddenData: PropTypes.object,
+  onValidate: PropTypes.func,
+  onSubmit: PropTypes.func,
   onResponse: PropTypes.func,
   onError: PropTypes.func,
 };
 
 DynamicFormWrapper.defaultProps = {
+  persistance: true,
   hiddenData: null,
+  onValidate: null,
+  onSubmit: null,
   onResponse: null,
   onError: null,
 };
